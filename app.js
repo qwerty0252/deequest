92df0688dc704cfa9b7ef34f993be31b6091e55a
qwerty0252 <enyinnanwukwa@gmail.com>
2023-07-21 15:58:49 +0100

    basically done

--- app.js
+++ app.js
@@ -1,5 +1,40 @@
 const http = require("http");
-const url_parser = require("url");
+const yargs = require("yargs");
+
+const argv = yargs
+  .option("get", {
+    alias: "g",
+    describe: "get method",
+  })
+  .option("post", {
+    alias: "p",
+    describe: "post method",
+  })
+  .option("delete", {
+    alias: "d",
+    describe: "delete method",
+  })
+  .option("put", {
+    alias: "pt",
+    describe: "put method",
+  })
+  .option("url", {
+    alias: "u",
+    describe: "request url",
+    demandOption: true,
+  })
+  .option("body", {
+    alias: "b",
+    describe: "data to be sent",
+  })
+  .help().argv;
+
+const arg_get = argv.get;
+const arg_post = argv.post;
+const arg_delete = argv.delete;
+const arg_put = argv.put;
+const arg_url = argv.url;
+const arg_body = argv.body;
 
 async function get(url) {
   return new Promise((resolve, reject) => {
@@ -58,4 +93,82 @@ async function post(url, body) {
   });
 }
 
-module.exports = { post, get };
+async function Delete(url) {
+  return new Promise((resolve, reject) => {
+    let data = "";
+    let options = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const request = http.request(url, options, (res) => {
+      res.setEncoding("utf8");
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      res.on("end", () => {
+        resolve(data);
+      });
+      res.on("error", (error) => {
+        reject(error);
+      });
+    });
+
+    request.on("error", (error) => {
+      reject(error);
+    });
+
+    request.end();
+  });
+}
+
+async function put(url, body) {
+  return new Promise((resolve, reject) => {
+    let put_body = JSON.stringify(body);
+    let data = "";
+    let options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Length": Buffer.byteLength(put_body),
+      },
+    };
+
+    const request = http.request(url, options, (res) => {
+      res.setEncoding("utf8");
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      res.on("end", () => {
+        resolve(data);
+      });
+      res.on("error", (error) => {
+        reject(error);
+      });
+    });
+
+    request.on("error", (error) => {
+      reject(error);
+    });
+
+    request.write(put_body);
+    request.end();
+  });
+}
+
+//This probably isn't the best way to this. But rn I'm brain dead. I'll think of a fancy way to this later
+if (arg_get) {
+  get(arg_url).then((res) => console.log(`response: ${res}`));
+} else if (arg_post) {
+  post(arg_url, arg_body).then((res) => console.log(`response: ${res}`));
+} else if (arg_delete) {
+  Delete(arg_url).then((res) => console.log(`response: ${res}`));
+} else if (arg_put) {
+  put(arg_url, arg_body).then((res) => console.log(`response: ${res}`));
+}
+
+module.exports = { post, get, delete: Delete, put };
